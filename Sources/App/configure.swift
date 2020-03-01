cdfedf85c952cd4076093b17fd9c6b580ca5c63a
Juan Cazalla Estrella <juan.cazalla@47deg.com>
2020-03-01 17:40:56 +0100

    Removes FluentSQL

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,7 @@
-import FluentSQLite
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
-    try services.register(FluentSQLiteProvider())
-
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -16,13 +12,5 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
-
-    // Register the configured SQLite database to the database config.
-    var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
-    services.register(databases)
 
 }
