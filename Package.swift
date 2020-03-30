daed558c7420b871b90e7445f2fdfb669e56e5cf
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-03-30 12:53:00 +0200

    update to Swift 5.2
    minor changes
    
    # Conflicts:
    #	Package.swift
    #	nef-editor-server.xcodeproj/xcshareddata/xcschemes/nef-editor-server-Package.xcscheme

--- Package.swift
+++ Package.swift
@@ -1,16 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "nef-editor-server",
+    platforms: [
+        .macOS(.v10_14),
+    ],
     products: [
         .library(name: "nef-editor-server", targets: ["App"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(name: "Vapor", url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(name: "nef", url: "https://github.com/bow-swift/nef.git", .branch("develop"))
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "nef"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
