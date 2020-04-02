5366e626d6f71d3ea4e531e0b0c954539a869577
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-04-02 12:54:56 +0200

    remove unuseful routes
    minor changes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,18 +3,13 @@ import Vapor
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register routers
-    let router = EngineRouter.default()
     let wss = NIOWebSocketServer.default()
-    try routes(router)
     try routes(wss)
-    
-    services.register(router, as: Router.self)
     services.register(wss, as: WebSocketServer.self)
     
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
-    
     services.register(middlewares)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,12 +1,6 @@
 import Vapor
 
-public func routes(_ router: Router) throws {
-    router.get { req in "It works!" }
-    router.get("hello") { req in "Hello, world!" }
-}
-
 public func routes(_ wss: NIOWebSocketServer) throws {
     let playgroundBookController = PlaygroundBookController()
-    
     wss.get("playground", use: playgroundBookController.handler)
 }
