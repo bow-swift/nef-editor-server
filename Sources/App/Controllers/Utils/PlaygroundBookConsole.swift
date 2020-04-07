326c40f95d5b81b34c92114858707a695f3dcbb7
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-04-07 13:23:23 +0200

    update progress models and use new fields in ProgressEvent
    comments in PR
    
    minor changes

--- Sources/App/Controllers/Utils/PlaygroundBookConsole.swift
+++ Sources/App/Controllers/Utils/PlaygroundBookConsole.swift
@@ -14,55 +14,35 @@ final class PlaygroundBookConsole: nef.ProgressReport, HasWebSocketOutput, HasCo
     }
     
     func notify<E: Swift.Error, A: CustomProgressDescription>(_ event: ProgressEvent<A>) -> IO<E, Void> {
-        guard let event = event as? ProgressEvent<PlaygroundBookEvent> else {
-            fatalError("received invalid event")
-        }
-        
-        return webSocket.send(command: PlaygroundBookCommand.Outgoing.status(event.playgroundBookStatus))
-                        .ignoreError()
-                        .provide(self)
+        webSocket.send(command: PlaygroundBookCommand.Outgoing.event(event.playgroundBookEvent))
+                 .ignoreError()
+                 .provide(self)
     }
 }
 
-private extension EnvIO where A == Void {
-    func ignoreError<E: Swift.Error, EE: Swift.Error>() -> EnvIO<D, EE, Void> where F == IOPartial<E> {
-        handleError { _ in }^.mapError { e in e as! EE }
-    }
-}
-
-private extension nef.ProgressEvent where A == PlaygroundBookEvent {
-    var playgroundBookStatus: PlaygroundBookStatus {
-        .init(step: .init(information: step.progressDescription,
-                          status: status.stepStatus),
-              progress: step.progressPercentage)
-    }
-}
-
-private extension nef.PlaygroundBookEvent {
-    var progressPercentage: Double {
-        let cases: Double = 5
-        
-        switch self {
-        case .cleanup:
-            return (1/cases) * 100
-        case .creatingStructure:
-            return (2/cases) * 100
-        case .downloadingDependencies:
-            return (3/cases) * 100
-        case .gettingModules:
-            return (4/cases) * 100
-        case .buildingPlayground:
-            return (5/cases) * 100
-        }
+// MARK: Helpers for extracting information from a PlaygroundBookEvent
+private extension nef.ProgressEvent {
+    var playgroundBookEvent: NefEditorData.PlaygroundBookEvent {
+        .init(information: step.progressDescription,
+              currentStep: step.currentStep,
+              totalSteps: step.totalSteps,
+              status: status.eventStatus)
     }
 }
 
 private extension nef.ProgressEventStatus {
-    var stepStatus: PlaygroundBookStatus.Status {
+    var eventStatus: NefEditorData.PlaygroundBookEvent.Status {
         switch self {
         case .inProgress: return .running
         case .successful: return .succesful
         case .failed: return .failed
         }
+    }
+}
+
+// MARK: helpers for ignoring the Error in EnvIO
+private extension EnvIO where A == Void {
+    func ignoreError<E: Swift.Error, EE: Swift.Error>() -> EnvIO<D, EE, Void> where F == IOPartial<E> {
+        handleError { _ in }^.mapError { e in e as! EE }
     }
 }
