d9d9f21b4e870741c800946591fc79caef9e9777
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-04-20 13:32:26 +0200

    using .env files to load secrets from Vapor environment

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,18 +4,41 @@ import AppleSignIn
 struct RouteRegister {
     let app: Application
     
-    func playgroundBook(config: @escaping (WebSocketOutput) -> PlaygroundBookConfig) {
+    func playgroundBook() throws {
         let controller = PlaygroundBookController(playgroundBook: PlaygroundBookServer(), config: config)
         app.webSocket("playgroundBook", onUpgrade: controller.handler)
     }
     
-    func appleSignIn() {
+    func appleSignIn() throws {
+        guard let p8Key = Environment.get("p8Key"),
+              let teamId = Environment.get("teamId"),
+              let keyId = Environment.get("keyId"),
+              let clientId = Environment.get("clientId"),
+              let redirectURI = Environment.get("redirectURI") else {
+                throw Abort(.internalServerError, reason: "credentials not found")
+        }
+
+        let environment = AppleSignInEnvironment(p8Key: p8Key, teamId: teamId, keyId: keyId, clientId: clientId, redirectURI: redirectURI)
         let client = AppleSignInClient()
         let apiConfig = API.Config(basePath: "https://appleid.apple.com")
             .appending(headers: ["Accept": "application/x-www-form-urlencoded",
                                  "Content-Type": "application/x-www-form-urlencoded"])
         
-        let controller = AppleSignInController(client: client, apiConfig: apiConfig)
+        let controller = AppleSignInController(client: client, apiConfig: apiConfig, environment: environment)
         app.post("signin", use: controller.handle)
     }
+    
+}
+
+
+// MARK: Builders
+private func config(webSocket: WebSocketOutput) -> PlaygroundBookConfig {
+    let output = URL(fileURLWithPath: NSTemporaryDirectory())
+    let encoder = JSONEncoder()
+    let decoder = JSONDecoder()
+    
+    return PlaygroundBookConfig(outputDirectory: output,
+                                encoder: encoder,
+                                decoder: decoder,
+                                webSocket: webSocket)
 }
