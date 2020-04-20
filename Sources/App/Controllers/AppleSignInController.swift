553233f45cc4f8a810daea7edd425585d2f4c5f1
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-04-20 11:10:40 +0200

    migrated to EnvIO.accessM some encode/decode actions

--- Sources/App/Controllers/AppleSignInController.swift
+++ Sources/App/Controllers/AppleSignInController.swift
@@ -28,55 +28,39 @@ final class AppleSignInController {
         
         return binding(
                 env <- .ask(),
-               body <- self.decode(body: request.content),
+               body <- self.decodeRequest(body: request.content),
            response <- env.get.client.signIn(body.get).contramap(\AppleSignInConfig.apiConfig),
             encoded <- self.encodeResponse(response.get),
         yield: encoded.get)^
     }
     
-    private func decode(body: ContentContainer) -> EnvIO<AppleSignInConfig, AppleSignInError, AppleSignInRequest> {
+    private func decodeRequest(body: ContentContainer) -> EnvIO<AppleSignInConfig, AppleSignInError, AppleSignInRequest> {
         EnvIO.invoke { _ in
             try body.decode(AppleSignInRequest.self)
         }.mapError { error in .decodingRequest(error) }^
     }
     
     private func encodeResponse(_ response: AppleSignInResponse) -> EnvIO<AppleSignInConfig, AppleSignInError, String> {
-        func encode(response: AppleSignInResponse, encoder: Encoder) -> EnvIO<AppleSignInConfig, AppleSignInError, String> {
-            encoder.safeEncode(response)^
+        EnvIO.accessM { env in
+            env.responseEncoder.safeEncode(response)^
                 .mapError { e in .encodingResponse(e) }^
                 .flatMap(self.encodeResponse)^
-        }
-        
-        let env = EnvIO<AppleSignInConfig, AppleSignInError, AppleSignInConfig>.var()
-        let encoded = EnvIO<AppleSignInConfig, AppleSignInError, String>.var()
-        
-        return binding(
-               env <- .ask(),
-           encoded <- encode(response: response, encoder: env.get.responseEncoder),
-        yield: encoded.get)^
+        }^
     }
     
     private func encodeResponse(_ response: Data) -> EnvIO<AppleSignInConfig, AppleSignInError, String> {
-        func encode(data: Data, encoder: Encoder) -> EnvIO<AppleSignInConfig, AppleSignInError, String> {
-            encoder.safeEncode(response)
-            .mapError { e in .encodingResponse(e) }^
-            .flatMap { encoded in
-                EnvIO.invoke { _ in
-                    guard let string = String(data: data, encoding: .utf8) else {
-                        throw AppleSignInError.invalidUTF8Encoding
-                    }
-                    
-                    return string
+        EnvIO.accessM { env in
+            env.responseEncoder.safeEncode(response)
+                .mapError { e in .encodingResponse(e) }^
+                .flatMap { encoded in
+                    EnvIO.invoke { _ in
+                        guard let string = String(data: response, encoding: .utf8) else {
+                            throw AppleSignInError.invalidUTF8Encoding
+                        }
+                        
+                        return string
+                    }^
                 }^
-            }^
         }
-        
-        let env = EnvIO<AppleSignInConfig, AppleSignInError, AppleSignInConfig>.var()
-        let encoded = EnvIO<AppleSignInConfig, AppleSignInError, String>.var()
-        
-        return binding(
-               env <- .ask(),
-           encoded <- encode(data: response, encoder: env.get.responseEncoder),
-        yield: encoded.get)^
     }
 }
