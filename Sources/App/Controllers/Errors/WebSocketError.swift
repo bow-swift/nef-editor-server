3b34e944ee06a9252aa2182e35ce83b2136d521b
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-05-11 15:37:00 +0200

    encode response - playground book

--- Sources/App/Controllers/Errors/WebSocketError.swift
+++ Sources/App/Controllers/Errors/WebSocketError.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum WebSocketError: Error {
     case encoding(error: Error)
-    case utf8Expected
+    case encodingUTF8
     case decoding(error: Error)
     case decoding(text: String)
     case sending(error: Error)
--- Sources/App/Controllers/Utils/WebSocket+Extensions.swift
+++ Sources/App/Controllers/Utils/WebSocket+Extensions.swift
@@ -9,7 +9,7 @@ extension WebSocket: WebSocketOutput {
     func send<D>(binary: Data) -> EnvIO<D, WebSocketError, Void> {
         EnvIO.invoke { _ in
             guard let message = String(data: binary, encoding: .utf8) else {
-                throw WebSocketError.utf8Expected
+                throw WebSocketError.encodingUTF8
             }
             
             self.send(message)
